6ff7115717e51d9d5278144b968b199edd75a37c
castlewr <112005799+castlewr@users.noreply.github.com>
2022-08-24 12:31:21 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/******************************************************************************* Hello.Java
+*******************************************************************************
+*William Castle
+*8/24/22
+*CMSC 255 
+******************************************************************************/
+
+public class Hello{
+	public static void main(String[] args) {
+
+		System.out.println("<William Castle>");
+		System.out.println("<Hello.Java>");
+		System.out.println("<Write your name in Java>");
+		System.out.println("<8/24/2022>");
+		System.out.println("<CMSC 255>");
+		System.out.println();
+	}
+}
